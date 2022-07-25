03cb53efcbc010ee162b3460aa788b7694b16cf8
@cyberduck1217 <rr.gps7620@gmail.com>
2022-07-25 19:22:15 +0530

    List using Local Files

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+
 class CatalogModel {
-  static final items = [
+  static List<Item> items = [
     Item(
         id: 1,
         name: "iPhone 12 Pro",
@@ -26,4 +30,24 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
+
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image,
+      };
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,21 +1,46 @@
-// ignore_for_file: prefer_const_constructors, unused_local_variable
+// ignore_for_file: prefer_const_constructors, unused_local_variable, avoid_print, unused_import, duplicate_import
+
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:signin_example/models/catalog.dart';
 import 'package:signin_example/widgets/drawer.dart';
+import 'dart:convert';
 
 import '../widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Rajat Thakur";
-
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final dummylist = List.generate(5, (index) => CatalogModel.items[0]);
+  State<Homepage> createState() => _HomepageState();
+}
 
+class _HomepageState extends State<Homepage> {
+  final int days = 30;
+
+  final String name = "Rajat Thakur";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
@@ -23,9 +48,9 @@ class Homepage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: dummylist.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: ((context, index) {
-            return ItemWidget(item: dummylist[index]);
+            return ItemWidget(item: CatalogModel.items[index]);
           }),
         ),
       ),
