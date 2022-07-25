cfa920b245992259bf3d94f9202e0e69e020d3da
@cyberduck1217 <rr.gps7620@gmail.com>
2022-07-25 14:54:48 +0530

    day 7

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,10 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_local_variable
 
 import 'package:flutter/material.dart';
+import 'package:signin_example/models/catalog.dart';
 import 'package:signin_example/widgets/drawer.dart';
+
+import '../widgets/item_widget.dart';
 
 class Homepage extends StatelessWidget {
   final int days = 30;
@@ -11,14 +14,19 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(5, (index) => CatalogModel.items[0]);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-      body: Center(
-        // ignore: avoid_unnecessary_containers
-        child: Container(
-          child: Text("Welcome to $days Days of Flutter $name "),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummylist.length,
+          itemBuilder: ((context, index) {
+            return ItemWidget(item: dummylist[index]);
+          }),
         ),
       ),
       drawer: MyDrawer(),
